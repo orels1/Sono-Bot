8fd95f18c7099f237d0a4c66dfedd6526e20a831
orels1 <orel-s1@yandex.ru>
2016-12-11 03:18:38 +0300

    db setup #2

--- app.js
+++ app.js
@@ -6,16 +6,22 @@ var logger = require('morgan');
 var bodyParser = require('body-parser');
 var express = require('express');
 
-var express = require('express');
-
 var app = express();
 
 // App Middleware
 app.set('port', process.env.PORT || 5000);
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ 'extended': false }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+// DB
+var mongoose = require('mongoose');
+mongoose.connect("localhost/sono");
+
+mongoose.connection.on('error', function() {
+    console.log('Error: Could not connect to MongoDB. Did you forget to run `mongod`?');
+});
 
 var server = require('http').createServer(app);
 server.listen(app.get('port'), function() {
