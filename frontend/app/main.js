4f2a0a46cb72ce884c23d6ac3aec1f41665ee012
orels1 <orel-s1@yandex.ru>
2016-12-25 05:15:19 +0300

    fixed react issues, got rid of extra history module

--- frontend/app/main.js
+++ frontend/app/main.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import Router from 'react-router';
-import ReactDOM from 'react-dom';
-import createHistory from 'history/createBrowserHistory';
+import { Router, browserHistory } from 'react-router';
+import { render } from 'react-dom';
+// import createHistory from 'history/createBrowserHistory';
 import routes from './routes';
 
 // This one is pure service module. It injects our markup into the page, shouldn't be changed
 
-let history = createHistory();
+// let history = createHistory();
 
-ReactDOM.render(<Router history={history}>{routes}</Router>, document.getElementById('app'));
+render((
+    <Router history={browserHistory}>{routes}</Router>
+    ), document.getElementById('app'));
