16e3cc400063953a8eba55b9ed8d728bf0b2f2a9
orels1 <orel-s1@yandex.ru>
2016-12-29 02:37:28 +0300

    minor tweaks and fixes

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     $('.container-table').height($(window).height());
-    $('.blog').height($(window).height());
+    $('.blog').css('minHeight', $(window).height() + 'px');
 
     $('#go-to-blog').click(function() {
         $('html, body').stop().animate({
